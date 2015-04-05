d295786b6afeeb350df6d185fda5886a051aae19
Evan Sandhoefner <evansandhoefner@college.harvard.edu>
2015-04-05 05:50:34 -0400

    fixed

--- hw3/js/countvis.js
+++ hw3/js/countvis.js
@@ -96,7 +96,7 @@ CountVis.prototype.initVis = function(){
         // Trigger selectionChanged event. You'd need to account for filtering by time AND type
         console.log(that.brush.extent());
         console.log(that);
-        // $(that.brushed).trigger("selectionChanged",brush.extent());
+        $(that.brushed).trigger("selectionChanged",brush.extent());
       });
 
     // Add axes visual elements
